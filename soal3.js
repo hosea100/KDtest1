60dd3e7d79e6630901ba23061288ffd827230680
Hosea Yoarana <hoseayoarana@gmail.com>
2023-09-12 15:27:32 +0700

    soal 3 clear

--- soal3.js
+++ soal3.js
@@ -13,7 +13,7 @@
 //   }
 // }
 // Output: 6
- 
+
 // Input:
 // {
 //   a: 2,
@@ -24,3 +24,61 @@
 // };
 // Output: 12
 
+function getSumOfEvenNumbers(obj) {
+  let sum = 0;
+
+  function traverse(obj) {
+    for (let key in obj) {
+      const value = obj[key];
+      if (typeof value === 'number' && value % 2 === 0) {
+        sum += value;
+      } else if (typeof value === 'object') {
+        traverse(value);
+      }
+    }
+  }
+
+  traverse(obj);
+  return sum;
+}
+
+const input1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup"
+    }
+  }
+}
+
+const input2 = {
+  a: 2,
+  b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+  c: { c: { c: 2 }, cc: 'ball', ccc: 5 },
+  d: 1,
+  e: { e: { e: 4 }, ee: 'car' }
+};
+
+const input3 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup",
+      thisIsANumberLmao: "2",
+      thisIsTheRealNumberSorry: 22,
+      subasuba: {
+        quack: 10,
+      }
+    }
+  }
+}
+
+console.log(getSumOfEvenNumbers(input1)); // 6
+console.log(getSumOfEvenNumbers(input2)); // 12
+console.log(getSumOfEvenNumbers(input3)); // 38
